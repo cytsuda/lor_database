957108f4cdc893bce2fa21fda8385b0b5c27c36b
Carlos Yoshio Tsuda <cyt231@gmail.com>
2021-02-22 18:45:39 -0300

    Updates

--- src/redux/actions/searchAction.tsx
+++
@@ -1,8 +0,0 @@
-import { AT, SEARCH_INPUT } from "../constants/searchConstants";
-
-export function searchAction(value: string): AT {
-  return {
-    type: SEARCH_INPUT,
-    payload: value,
-  };
-}
--- src/redux/constants/searchConstants.tsx
+++
@@ -1,8 +0,0 @@
-export const SEARCH_INPUT = "SEARCH_INPUT";
-
-interface SI_AT {
-  type: typeof SEARCH_INPUT;
-  payload: string;
-}
-
-export type AT = SI_AT;
